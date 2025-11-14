1c0f4c6a9129e30799d4fd67c4f6b9d2f9ebda5e
Laura <laurapigar222@gmail.com>
2025-11-14 13:49:18 +0100

    Actividad 5.4. MultMov

--- app/src/main/java/com/example/controlesbasicos/Activity54.java
+++ app/src/main/java/com/example/controlesbasicos/Activity54.java
@@ -1,6 +1,10 @@
 package com.example.controlesbasicos;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +15,27 @@ public class Activity54 extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         //Enlazamos el XML con la actividad
         setContentView(R.layout.activity_5_4);
+
+        EditText editTextPhone = findViewById(R.id.editTextPhone);
+        EditText editTextNumber = findViewById(R.id.editTextNumber);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextPin = findViewById(R.id.editTextNumberPassword);
+
+        enableKeyboardOnFocus(editTextNumber);
+        enableKeyboardOnFocus(editTextEmail);
+        enableKeyboardOnFocus(editTextPin);
+        enableKeyboardOnFocus(editTextPhone);
+    }
+    private void enableKeyboardOnFocus(EditText editText) {
+        editText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                editText.requestFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+                return false; // permitir que el evento continúe normalmente
+            }
+        });
+
     }
 }
